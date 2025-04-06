58c9b0d6f59119462156936d466f72237348b74e
vegaalfaro <vegaalfaro@wisc.edu>
2025-04-06 18:54:58 -0500

    add

---
+++ R/simulated_geno.R
@@ -0,0 +1,12 @@
+#' Simulated Simple Genotype Matrix Dataset
+#'
+#' This dataset contains a sample genotype matrix for demonstration of `geneticMapper` functions.
+#'
+#' @format A data frame with 6 rows and 5 variables:
+#' \describe{
+#'   \item{Parent1}{Parent1 genotype in dosage of ALT allele (numeric)}
+#'   \item{Parent2}{Parent2 genotype in dosage of ALT allele (numeric)}
+#'   \item{F2_1}{Progeny genotype in dosage of ALT allele (numeric)}
+#' }
+#' @source Simulated data for internal use.
+"simulated_geno"
